991f8749e42b1ffb1546d8f59f89808a0e6d678f
Abhinaya Balaji <abijun4@gmail.com>
2022-12-02 14:56:50 -0800

    Inheritance added

---
+++ com/perscholas/oop/com/perscholas/oop/inheritance/Surgeon.java
@@ -0,0 +1,10 @@
+package com.perscholas.oop.inheritance;
+
+class Surgeon extends Doctor {
+    void Surgeon_Details() {
+        System.out.println("Surgeon  Detail...");
+        System.out.println(Department = "Cardio");
+    }
+}
+
+
